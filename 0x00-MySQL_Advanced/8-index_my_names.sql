491975dbf13bdb516dec55298b6ec4296f254073
timontuitoek <tuitoektimon@gmail.com>
2024-03-25 23:13:02 +0300

    An SQL script that creates an index idx_name_first on the table names and the first letter of name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@ Task: Create an index idx_name_first on the table names and the first letter of 
 */
 
 -- Create index idx_name_first
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1));
