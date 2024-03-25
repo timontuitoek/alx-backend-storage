67f1d6c72a137018b14f5213f4bac73f7d83aa20
timontuitoek <tuitoektimon@gmail.com>
2024-03-25 23:14:31 +0300

    An SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,6 @@
+/*
+Task: Create an index idx_name_first_score on the table names including the first letter of name and the score
+*/
+
+-- Create index idx_name_first_score
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
