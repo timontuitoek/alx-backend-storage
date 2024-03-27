ff489b7672e7aa75266bdcc02d1ec8ee480c72ae
timontuitoek <tuitoektimon@gmail.com>
2024-03-27 23:57:22 +0300

    Redis file and a Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
 
     def __init__(self) -> None:
         """
-        Initialize the Cache object with a Redis client and flush the Redis instance.
+        Initialize the Cache object
         """
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
@@ -24,4 +24,3 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
